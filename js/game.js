623a760c340217444085bd44c047976e339283b5
KrishnaP <kpat132@gmail.com>
2018-01-25 19:03:48 -1000

    spaceship

--- js/game.js
+++ js/game.js
@@ -1,21 +1,26 @@
 (Phaser => {
   console.log(Phaser);
-  const GAME_WIDTH = 460;
-  const GAME_HEIGHT = 600;
+  const GAME_WIDTH = 400;
+  const GAME_HEIGHT = 400;
   const GAME_CONTAINER_ID = 'game';
 
+  const GFX = 'gfx';
+  let player;
+
   const game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, GAME_CONTAINER_ID, { preload, create, update });
+ 
 
   function preload() {
+    game.load.spritesheet(GFX, '../assets/shmup-spritesheet-140x56-28x28-tile.png', 28, 28);
 
   };
 
   function create() {
-
+    player = game.add.sprite(100,100,GFX,8);
   };
 
   function update() {
-    
+
   };
 
 })(window.Phaser);
