8a8402ad594063c40b4d3b43895b777a370a27fa
KrishnaP <kpat132@gmail.com>
2018-01-25 19:16:28 -1000

    spaceship moves

--- js/game.js
+++ js/game.js
@@ -3,9 +3,11 @@
   const GAME_WIDTH = 400;
   const GAME_HEIGHT = 400;
   const GAME_CONTAINER_ID = 'game';
-
   const GFX = 'gfx';
+  const INITIAL_MOVESPEED = 4;
+
   let player;
+  let cursors;
 
   const game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, GAME_CONTAINER_ID, { preload, create, update });
  
@@ -16,11 +18,33 @@
   };
 
   function create() {
+    cursors = game.input.keyboard.createCursorKeys();
     player = game.add.sprite(100,100,GFX,8);
+    player.moveSpeed = INITIAL_MOVESPEED;
   };
 
   function update() {
+    handlePlayerMovement();
+  };
 
+  //handler function
+  function handlePlayerMovement() {
+    switch( true ){
+      case cursors.left.isDown:
+        player.x -= player.moveSpeed;
+        break;
+      case cursors.right.isDown:
+        player.x += player.moveSpeed;
+        break;
+    }
+    switch( true ){
+      case cursors.down.isDown:
+        player.y += player.moveSpeed;
+        break;
+      case cursors.up.isDown:
+        player.y -= player.moveSpeed;
+        break;
+    }
   };
 
 })(window.Phaser);
